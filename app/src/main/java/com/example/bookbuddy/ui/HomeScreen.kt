a4ce8c59e22380544beb510aba47f432c6eed4eb
Tushar <guptatushar193@gmail.com>
2024-08-12 01:00:23 +0530

    snapping effect

--- app/src/main/java/com/example/bookbuddy/ui/HomeScreen.kt
+++ app/src/main/java/com/example/bookbuddy/ui/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.bookbuddy.ui
 
 import android.util.Log
 import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
@@ -26,6 +28,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -52,6 +55,9 @@ import com.example.bookbuddy.ui.util.CarouselPager
 import com.example.bookbuddy.ui.util.CustomSearchBar
 import com.example.bookbuddy.ui.util.HomeScreenTopBar
 import com.example.compose.BookBuddyTheme
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
 
@@ -120,19 +126,18 @@ fun EmptyScreen(modifier: Modifier) {
 }
 @Composable
 fun HomeViewContent(homeUiState: HomeUiState.HomeView, modifier: Modifier = Modifier) {
+    val spacer= with(LocalDensity.current){50.dp.toPx()*this.density.toInt()}
+    val height = min(400.dp, LocalConfiguration.current.screenHeightDp.dp * 0.6f)
+
+
     val lazyListState = rememberLazyListState()
     val coroutineScope = rememberCoroutineScope()
-    val isSnapped by remember{ derivedStateOf { lazyListState.firstVisibleItemIndex>0 } }
-    val height = with(LocalDensity.current){
-        min(400.dp, LocalConfiguration.current.screenHeightDp.dp*0.45f)
-    }
 
-    val alpha by animateFloatAsState(
-        targetValue = if(isSnapped) 0f else 1f,
-        label = " Carousel Snap",
-        animationSpec = tween(durationMillis = 1000, easing = EaseOut)
-    )
-    val stop = with(LocalDensity.current){ 50.dp.toPx()}
+
+    var previousIndex by remember { mutableIntStateOf(0) }
+    var previousScrollOffset by remember { mutableIntStateOf(0) }
+
+
     LazyColumn(
         state = lazyListState,
         modifier = modifier,
@@ -140,28 +145,17 @@ fun HomeViewContent(homeUiState: HomeUiState.HomeView, modifier: Modifier = Modi
             horizontal = dimensionResource(id = R.dimen.medium_padding),
             vertical = dimensionResource(id = R.dimen.large_padding)
         )
-    ){
-
-        item{
+    ) {
+        item {
             Box(
                 modifier = Modifier
                     .height(height)
                     .fillMaxWidth()
-                    .alpha(alpha)
-                    .graphicsLayer {
-                        lerp(
-                            start = 0f,
-                            stop = stop.absoluteValue,
-                            fraction = 1-alpha
-                        ).also { translationY = it }
-                    }
-            ){
+            ) {
                 CarouselPager(
                     books = homeUiState.carauselBooks,
                     onDrag = {},
-                    modifier = Modifier
-                        .height(height)
-                        .fillMaxWidth()
+                    modifier = Modifier.fillMaxSize()
                 )
             }
             Spacer(Modifier.height(40.dp))
@@ -169,26 +163,35 @@ fun HomeViewContent(homeUiState: HomeUiState.HomeView, modifier: Modifier = Modi
         items(items = homeUiState.bookList, key = { it.id }) { book ->
             BookCard(
                 book = book,
-                onClick = {  },
-                onLongPress = {  },
+                onClick = {},
+                onLongPress = {},
                 diskCachePolicy = CachePolicy.DISABLED,
                 memoryCachePolicy = CachePolicy.ENABLED,
                 modifier = Modifier.height(150.dp)
             )
         }
     }
-    val px = with(LocalDensity.current){ height*0.7f.dp.toPx()}
-    val px2 = with(LocalDensity.current){ height*0.2f.dp.toPx()}
-    LaunchedEffect(Unit){
-        snapshotFlow { lazyListState.firstVisibleItemScrollOffset }
-            .collect{offset->
-                if(offset> px2.value && !isSnapped ){
+
+    LaunchedEffect(lazyListState) {
+        snapshotFlow { lazyListState.firstVisibleItemIndex to lazyListState.firstVisibleItemScrollOffset }
+            .filter {(currentIndex,_)-> currentIndex<2 }
+            .distinctUntilChanged()
+            .collect { (currentIndex, currentScrollOffset) ->
+                val isScrollingUp = if (currentIndex != previousIndex) {
+                    currentIndex < previousIndex
+                } else {
+                    currentScrollOffset <= previousScrollOffset
+                }
+
+                if (currentScrollOffset > 0 && currentIndex == 0 && !isScrollingUp) {
                     coroutineScope.launch { lazyListState.animateScrollToItem(1) }
-                }else if(offset.toFloat() == px.value&& isSnapped){
-                    Log.d("inverse Scroll","true")
+                } else if (currentIndex == 1 && isScrollingUp && currentScrollOffset-previousScrollOffset> spacer) {
                     coroutineScope.launch { lazyListState.animateScrollToItem(0) }
                 }
-                Log.d("inverse offset", "$offset   ${px.value}  ${offset < px.value}")
+
+                previousIndex = currentIndex
+                previousScrollOffset = currentScrollOffset
+
             }
     }
 }
